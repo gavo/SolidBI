5e2aa3c6bd797cac5fcd9d477c009d76617249b7
Gustavo Vargas Miranda <gvmgust@gmail.com>
2025-05-10 14:59:25 -0400

    GetServicioChangesByRowVersion procedure added

---
+++ SolidOLTP/Schema/Programmability/GetServicioChangesByRowVersion.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetServicioChangesByRowVersion]
+(@startRow BIGINT, @endRow BIGINT) AS 
+BEGIN
+	SELECT 
+		s.id AS ServicioID,
+		s.codigo,
+		s.nombre_servicio,
+		s.precio
+	FROM servicios AS s
+	WHERE s.rowversion > CONVERT(ROWVERSION, @startRow)
+	AND s.rowversion <= CONVERT(ROWVERSION, @endRow)
+END 
+GO
