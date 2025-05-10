3e468d767e5be93b81b64c67e603b45fe92ee569
Gustavo Vargas Miranda <gvmgust@gmail.com>
2025-05-10 04:50:05 -0400

    procedures added to OLTP

---
+++ SolidOLTP/Schema/Programmability/GetClienteChangesByRowVersion.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetClienteChangesByRowVersion]
+(
+	@startRow BIGINT
+	,@endRow BIGINT
+)
+AS
+BEGIN
+	SELECT 
+		c.id, code, documento, email, nombre_cliente, tipo_documento,
+		gc.nombre_grupo_cliente, gc.descuento
+	FROM clientes c LEFT JOIN grupo_cliente gc
+	ON c.id_grupo_clientes = gc.id
+	WHERE c.rowversion > CONVERT(ROWVERSION, @startRow)
+	AND c.rowversion <= CONVERT(ROWVERSION, @endRow)
+END
+GO
